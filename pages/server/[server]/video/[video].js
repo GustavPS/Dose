da245c0a4351f6b9232b0824d047be9eb74ad2b1
Gustav P Svensson <gustavps97@gmail.com>
2020-05-13 18:05:28 +0200

    Seeking now completly works :)

--- pages/server/[server]/video/[video].js
+++ pages/server/[server]/video/[video].js
@@ -30,24 +30,48 @@ export default function Home() {
 
   useEffect(() => {
     video = videojs("video");
-    video.src('http://localhost:4000/api/video/sdf');
+    video.src({
+      src: 'http://localhost:4000/api/video/sdf',
+      type: 'video/webm'
+    });
+
 
      // hack duration
      video.duration= function() {return video.theDuration; };
      video.start= 0;
-     video.oldCurrentTime= video.currentTime;
+
+     // The original code for "currentTime"
+     video.oldCurrentTime = function currentTime(seconds) {
+      if (typeof seconds !== 'undefined') {
+        if (seconds < 0) {
+          seconds = 0;
+        }
+
+        this.techCall_('setCurrentTime', seconds);
+        return;
+      }
+      this.cache_.currentTime = this.techGet_('currentTime') || 0;
+      return this.cache_.currentTime;
+    }
+
+      // Our modified currentTime
      video.currentTime= function(time) 
      { 
          if( time == undefined )
          {
-             return video.oldCurrentTime + video.start;
+             return video.oldCurrentTime() + video.start;
          }
          video.start= time;
-         video.oldCurrentTime = 0;
-         video.src("http://localhost:4000/api/video/sdf?start=" + time);
+         video.oldCurrentTime(0);
+         video.src({
+           src: "http://localhost:4000/api/video/sdf?start=" + time,
+           type: 'video/webm'
+          });
          video.play();
          return this;
      };
+
+     // Get the dureation of the movie
      $.getJSON( "http://localhost:4000/api/video/sdf/getDuration", function( data ) 
      {
          video.theDuration= data.duration;
@@ -61,8 +85,8 @@ export default function Home() {
     <>
         <Head>
         <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-        <link href="//vjs.zencdn.net/4.5/video-js.css" rel="stylesheet" />
-    <script src="//vjs.zencdn.net/4.5/video.js"></script>
+        <link href="https://vjs.zencdn.net/7.7.6/video-js.css" rel="stylesheet" />
+        <script src="https://vjs.zencdn.net/7.7.6/video.js"></script>
 
         </Head>
         <video id="video" className="video-js vjs-default-skin" controls preload="auto" width="640" height="264">
