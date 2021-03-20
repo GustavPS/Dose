8d7ced1f359a16fff951f33970857d698a5db2c6
Filip Eriksson <f.eriksson.21.fe@gmail.com>
2021-03-20 16:22:05 +0100

    started to work on watch list thingy

---
+++ MovieServer/movieserver/pages/api/movies/[movie]/addToWatchList.js
@@ -0,0 +1,32 @@
+const db = require('../../../../lib/db');
+const cors = require('../../../../lib/cors');
+const validateUser = require('../../../../lib/validateUser');
+
+export default (req, res) => {
+  return new Promise(resolve => {
+        res = cors(res);
+        let movieID = req.query.movie;
+        let token = req.query.token;
+        let goingToAdd = req.query.add;
+        let decoded = validateUser(token)
+
+        if (!decoded) {
+            res.status(403).end();
+            resolve();
+            return;
+        }
+        console.log("called addtowatch with " + goingToAdd)
+        let user_id = decoded.user_id;
+        if (goingToAdd == "true" || goingToAdd) {
+            db.none('INSERT INTO user_movie_watchlist (user_id, movie_id) VALUES ($1, $2) ON CONFLICT DO nothing', [user_id, movieID]);
+        } else {
+            db.none('DELETE FROM user_movie_watchlist (user_id, movie_id) VALUES ($1, $2)', [user_id, movieID]);
+        }
+
+        res.status(200).json({
+            success: true
+        });
+        resolve();
+  });
+}
+  
