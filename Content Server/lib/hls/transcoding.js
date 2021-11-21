16949e90f691d5a7e74d8bc67d6f57ce4c3b2ce3
Gustav P Svensson <gustavps97@gmail.com>
2021-11-21 20:53:58 +0100

    [Fix] Fix crash when preparing some codecs for directplay

--- Content Server/lib/hls/transcoding.js
+++ Content Server/lib/hls/transcoding.js
@@ -150,11 +150,15 @@ class Transcoding {
         ];
     }
 
-    prepareDirectplay(output) {
+    prepareDirectplay(output, codec) {
         return new Promise((resolve, reject) => {
+            if (codec == "mpeg4") { // Mpeg4 = h264
+                codec = "h264";
+            }
             this.output = path.join(output, 'result.m3u8');
             const outputOptions = this.getOutputOptions();
             const inputOptions = [
+                `-f ${codec}`,
                 '-copyts',
                 '-threads 8',
                 '-fflags +genpts', // Fixes AVI issues
