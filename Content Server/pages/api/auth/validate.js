1fe851ab9b37a8b690c2fa639a588486185bdc83
Gustav P Svensson <gustavps97@gmail.com>
2022-03-06 17:43:21 +0100

    [Fix] Check has_access on the server side

--- Content Server/pages/api/auth/validate.js
+++ Content Server/pages/api/auth/validate.js
@@ -11,7 +11,7 @@ const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
 export default async (req, res) => {
     return new Promise(async (resolve) => {
-    res = cors(res);
+        res = cors(res);
         await fetch(`${config.mainServer}/api/auth/validate`, {
             method: 'POST',
             headers: {
@@ -21,48 +21,57 @@ export default async (req, res) => {
                 token: req.body.token,
             }),
         })
-        .then((r) => r.json())
-        .then((data) => {
-            if (data.valid) {
-                db.any('SELECT id FROM users WHERE username = $1', [data.username])
-                    .then(user => {
-                        if(user.length == 1) {
-                            user = user[0];
+            .then((r) => r.json())
+            .then((data) => {
+                if (data.valid) {
+                    db.any('SELECT id, has_access FROM users WHERE username = $1', [data.username])
+                        .then(user => {
+                            if (user.length == 1) {
+                                user = user[0];
 
-                            const expiresIn = parseInt(process.env.ACCESS_TOKEN_VALID_TIME);
-                            const token = jwt.sign(
-                                {
-                                    username: data.username,
-                                    user_id: user.id
-                                },
-                                process.env.SECRET,
-                                {
-                                    expiresIn: expiresIn
-                                },
-                            );
-                            const validTo = Math.round((Date.now() / 1000) + expiresIn);
+                                if (!user.has_access) {
+                                    res.status(403).json({
+                                        success: false,
+                                        error: 'User does not have access',
+                                    });
+                                    resolve();
+                                    return;
+                                }
 
-                            res.status(200).json({
-                                status: 'success',
-                                token: token,
-                                validTo: validTo
-                            });
-                            resolve();
-                        } else {
-                            res.status(200).json({
-                                status: 'error',
-                                error: 'User not found in database'
-                            });
-                            resolve();
-                        }
-                });
-            } else {
-                res.status(200).json({
-                    status: 'error',
-                    error: 'Not a valid token'
-                })
-                resolve();
-            }
-        });
-  });
+                                const expiresIn = parseInt(process.env.ACCESS_TOKEN_VALID_TIME);
+                                const token = jwt.sign(
+                                    {
+                                        username: data.username,
+                                        user_id: user.id
+                                    },
+                                    process.env.SECRET,
+                                    {
+                                        expiresIn: expiresIn
+                                    },
+                                );
+                                const validTo = Math.round((Date.now() / 1000) + expiresIn);
+
+                                res.status(200).json({
+                                    status: 'success',
+                                    token: token,
+                                    validTo: validTo
+                                });
+                                resolve();
+                            } else {
+                                res.status(200).json({
+                                    status: 'error',
+                                    error: 'User not found in database'
+                                });
+                                resolve();
+                            }
+                        });
+                } else {
+                    res.status(200).json({
+                        status: 'error',
+                        error: 'Not a valid token'
+                    })
+                    resolve();
+                }
+            });
+    });
 }
