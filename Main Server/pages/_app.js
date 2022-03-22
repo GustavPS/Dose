4115edbf4247da193183913fd21c021f155311c7
Gustav P Svensson <gustavps97@gmail.com>
2022-03-22 22:27:27 +0100

    Improve main server security (#79)

--- Main Server/pages/_app.js
+++ Main Server/pages/_app.js
@@ -12,10 +12,10 @@ DoseApp.getInitialProps = async (appContext) => {
     const cookies = new Cookies(appContext.ctx.req, appContext.ctx.res)
     // calls page's `getInitialProps` and fills `appProps.pageProps`
     const appProps = await App.getInitialProps(appContext);
- 
+
     // Allowed pages without auth
     const noAuthPages = ['/login', '/_error'];
- 
+
     for (const noAuthPage of noAuthPages) {
         if (appContext.ctx.pathname == noAuthPage) {
             return { ...appProps }
@@ -23,64 +23,72 @@ DoseApp.getInitialProps = async (appContext) => {
     }
 
     // Only runs server side
-   if (appContext.ctx.res) {
+    if (appContext.ctx.res) {
         if (!tokenExist(appContext.ctx.req.cookies)) {
-            console.log("No token saved");
             return redirectToLogin(appContext.ctx.res);
         }
 
-       let accessToken  = appContext.ctx.req.cookies.token;
-       let refreshToken = appContext.ctx.req.cookies.refreshToken;
-       let validTo      = appContext.ctx.req.cookies.validTo;
+        let accessToken = appContext.ctx.req.cookies.token;
+        let refreshToken = appContext.ctx.req.cookies.refreshToken;
+        let validTo = appContext.ctx.req.cookies.validTo;
 
-       const currentTime      = Date.now() / 1000;
-       const accessTokenValid = (validTo - currentTime) >= 60;
+        const currentTime = Date.now() / 1000;
 
-       console.log("Access token valid: " + accessTokenValid);
+        let accessTokenLegitimate = await fetch(`http://localhost:${process.env.SERVER_PORT}${process.env.SERVER_SUB_FOLDER}/api/auth/validate`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                token: accessToken,
+            })
+        });
+        accessTokenLegitimate = await accessTokenLegitimate.json();
+        const accessTokenValid = (validTo - currentTime) >= 60 && accessTokenLegitimate.valid;
 
-       if (accessTokenValid) {
-           return { ...appProps };
-       }
+        if (accessTokenValid) {
+            return { ...appProps };
+        }
 
-       // If access token has expired or is about to expire
-       const req = await fetch(`http://localhost:${process.env.SERVER_PORT}${process.env.SERVER_SUB_FOLDER}/api/auth/refreshToken`, {
-           method: 'POST',
-           headers: {
-               'Content-Type': 'application/json'
-           },
-           body: JSON.stringify({
-               token: accessToken,
-               refreshToken: refreshToken
-           })
-       });
+        // If access token has expired or is about to expire
+        const req = await fetch(`http://localhost:${process.env.SERVER_PORT}${process.env.SERVER_SUB_FOLDER}/api/auth/refreshToken`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                token: accessToken,
+                refreshToken: refreshToken
+            })
+        });
 
-       const newTokenInfo = await req.json();
-       if (newTokenInfo.status === 'success') {
-           console.log(newTokenInfo);
-           saveTokenInfo(cookies,
-                         newTokenInfo.token,
-                         newTokenInfo.refreshToken,
-                         newTokenInfo.validTo);
-           console.log("Refreshed token");
-           return { ...appProps };
-       } else {
-           clearTokenInfo(cookies);
-           console.log("Couldn't get new token");
-           return redirectToLogin(appContext.ctx.res);
-       }
-   } else {
-       console.log("klient");
-       return { ...appProps }
-   }
+        const newTokenInfo = await req.json();
+        if (newTokenInfo.status === 'success') {
+            console.log(newTokenInfo);
+            saveTokenInfo(cookies,
+                newTokenInfo.token,
+                newTokenInfo.refreshToken,
+                newTokenInfo.validTo);
+            console.log("Refreshed token");
+            return { ...appProps };
+        } else {
+            clearTokenInfo(cookies);
+            console.log("Couldn't get new token");
+            return redirectToLogin(appContext.ctx.res);
+        }
+    } else {
+        console.log("klient");
+        return { ...appProps }
+    }
 }
 
 function tokenExist(cookies) {
-    let accessToken  = cookies.token;
+    let accessToken = cookies.token;
     let refreshToken = cookies.refreshToken;
-    let validTo      = cookies.validTo;
-    return accessToken  != null  && accessToken != undefined &&
-           refreshToken != null && refreshToken != undefined &&
-           validTo      != null && validTo      != undefined;
+    let validTo = cookies.validTo;
+    return accessToken != null && accessToken != undefined &&
+        refreshToken != null && refreshToken != undefined &&
+        validTo != null && validTo != undefined;
 }
 
 function redirectToLogin(res) {
